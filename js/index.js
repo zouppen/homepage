8ebbe1f32fc1ea323301cde997e373881ae9047a
Juho Vepsäläinen <bebraw@gmail.com>
2012-08-29 22:54:37 +0300

    Tidy up index.js

--- js/index.js
+++ js/index.js
@@ -9,18 +9,5 @@ require(['jquery', 'utils', 'rss', 'twitter', 'jquery.caro', 'jquery.yabox'],
         utils.pick($('#motto'), mottos);
         rss.widget($('#news'), feeds, 5);
         twitter.widget($('#tweets'), twitterUsers, 5);
-
-        $('#contact').yabox({
-            hideOnClick: false,
-            $content: $('#contactForm')
-        });
-        $('#contactForm .close').bind('click', $('#contact').yabox.hide);
-
-        /* bg pan test
-        $('.hero-unit').on('mousemove', function(e) {
-            var y = e.pageY - this.offsetTop;
-
-            $(this).css('background-position-y', -y + 'px');
-        });*/
     });
 });
